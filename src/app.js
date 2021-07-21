008688ede3cae196a61efaebeff7a187cdf6fdcd
chenyanquan <514324622@qq.com>
2021-07-21 23:00:58 +0800

    增加session功能

--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
-const Koa = require('koa')
-const app = new Koa()
-const views = require('koa-views')
-const json = require('koa-json')
-const onerror = require('koa-onerror')
-const bodyparser = require('koa-bodyparser')
-const logger = require('koa-logger')
-
-const index = require('./routes/index')
-const users = require('./routes/users')
-
+const Koa = require('koa');
+const app = new Koa();
+const views = require('koa-views');
+const json = require('koa-json');
+const onerror = require('koa-onerror');
+const bodyparser = require('koa-bodyparser');
+const logger = require('koa-logger');
+const session = require('koa-generic-session');
+const redisStore = require('koa-redis');
+const index = require('./routes/index');
+const users = require('./routes/users');
+const { REDIS_CONF } = require('./conf/db');
 // error handler
 onerror(app)
 
@@ -16,6 +17,21 @@ onerror(app)
 app.use(bodyparser({
   enableTypes:['json', 'form', 'text']
 }))
+// session 配置
+app.keys = ['Uisdf_50327'];
+app.use(session({
+  key: 'wb.sid', // cookie name 默认是  koa.sid
+  prefix: 'wb:sess:', // redis key的前缀   默认 koa:sess:
+  cookie: {
+    path: '/', //根目录
+    httpOnly: true, //只能 server 来修改 cookie
+    maxAge:  60 * 60 * 1000 // ms
+  },
+  // ttl: 60 * 60 * 1000, //redis 过期时间 默认maxAge 时间一致
+  store: redisStore({
+    all: REDIS_CONF.host + ':' + REDIS_CONF.port
+  }) //存储到redis中
+}));
 app.use(json())
 app.use(logger())
 app.use(require('koa-static')(__dirname + '/public'))
